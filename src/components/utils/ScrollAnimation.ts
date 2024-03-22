a803b6f2f6f7eae1a47af4e9b085ce1fe54b9331
Andresfrla <125943749+Andresfrla@users.noreply.github.com>
2024-03-22 13:52:43 -0600

    add animations to the portfolio

---
+++ src/components/utils/ScrollAnimation.ts
@@ -0,0 +1,22 @@
+function handleVisibility(entries, observer) {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            const classesToAdd = ['animate-fade-right', 'animate-once', 'animate-duration-[3000ms]'];
+            classesToAdd.forEach(className => {
+                entry.target.classList.add(className);
+            });
+        } else {
+            const classesToRemove = ['animate-fade-right', 'animate-once', 'animate-duration-[3000ms]'];
+            classesToRemove.forEach(className => {
+                entry.target.classList.remove(className);
+            });
+        }   
+    });
+}  
+
+let observer = new IntersectionObserver(handleVisibility, {});
+
+let elements = document.getElementsByClassName("animate-scroll");
+Array.from(elements).forEach(element => {
+    observer.observe(element);
+});
